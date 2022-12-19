6ae2e233927543d4b84b2c408623186ddbd76ccd
Zheng <aheze@getfind.app>
2022-12-18 20:12:10 -0800

    Add base

--- README.md
+++ README.md
@@ -4,5 +4,19 @@ Website for sharing filters from Find.
 
 https://lists.getfind.app/list?title=List&description=This%20is%20a%20description&icon=message.circle&color=4284849459&dateCreated=2022-03-31T15%3A56%3A14.898Z&words=Special%20Characters%21%21%21%20%40aheze%2C%20100%25,Some%20word,Word%20with%2C%20comma,A%20word
 
+
+https://filters.getfind.app/filter?title=Work&data=%7B%22title%22:%22Work%22,%22icon%22:%22briefcase%22,%22location%22:%7B%22regionSpan%22:280,%22coordinate%22:%7B%22longitude%22:-122.37927868282958,%22latitude%22:37.616220590518338%7D,%22locationAddress%22:null,%22paddingRadius%22:0,%22locationTitle%22:%22San%20Francisco%20International%20Airport%22%7D%7D
+
+https://filters.getfind.app/filter?kind=location&title=Home&data=%7B%22title%22:%22Home%22,%22icon%22:%22house%22,%22location%22:%7B%22regionSpan%22:150,%22coordinate%22:%7B%22longitude%22:-77.035939716259662,%22latitude%22:38.897876677177138%7D,%22locationAddress%22:null,%22paddingRadius%22:0,%22locationTitle%22:%22The%20White%20House%22%7D%7D
+
+
 Clicking the link should open Find v1.3. Find v1.3 should be coming by April 10th.
 
+
+findapp://?type=filter&data=%7B%22title%22%3A%22Home%22%2C%22icon%22%3A%22house%22%2C%22location%22%3A%7B%22regionSpan%22%3A150%2C%22coordinate%22%3A%7B%22longitude%22%3A-77.035939716259662%2C%22latitude%22%3A38.897876677177138%7D%2C%22locationAddress%22%3Anull%2C%22paddingRadius%22%3A0%2C%22locationTitle%22%3A%22The%20White%20House%22%7D%7D
+
+
+https://filters.getfind.app/filter?kind=location&title=Work&data=%7B%22title%22:%22Work%22,%22icon%22:%22briefcase%22,%22location%22:%7B%22regionSpan%22:280,%22coordinate%22:%7B%22longitude%22:-122.37927868282958,%22latitude%22:37.616220590518338%7D,%22locationAddress%22:null,%22paddingRadius%22:0,%22locationTitle%22:%22San%20Francisco%20International%20Airport%22%7D%7D
+
+
+findapp://?type=filter&kind=location&data=%7B%22title%22%3A%22Work%22%2C%22icon%22%3A%22briefcase%22%2C%22location%22%3A%7B%22regionSpan%22%3A280%2C%22coordinate%22%3A%7B%22longitude%22%3A-122.37927868282958%2C%22latitude%22%3A37.616220590518338%7D%2C%22locationAddress%22%3Anull%2C%22paddingRadius%22%3A0%2C%22locationTitle%22%3A%22San%20Francisco%20International%20Airport%22%7D%7D
--- filter.js
+++ filter.js
@@ -5,22 +5,8 @@
 var r = document.querySelector(':root');
 
 const applink = document.getElementById("applink");
-const checkbox = document.getElementById("check");
 const titleElement = document.getElementById("title");
 const descriptionElement = document.getElementById("description");
-const wordsList = document.getElementById("words");
-
-const wordsContainer = document.getElementById("wordsContainer");
-const updateCreationDateCheckbox =
-    document.getElementById("updateCreationDate");
-
-let originalSearch = window.location.search.substring(1);
-
-let currentState = history.state;
-if (currentState) {
-    originalSearch = currentState.originalSearch;
-}
-
 
 /**
  * Get the search parameters
@@ -45,82 +31,23 @@ const getSearchQueries = (search) => {
     return queries;
 };
 
-/**
- * Generate a query string from an array of parameters
- * @param {SearchParam[]} queries - Query string parameters (generated from {@link getSearchQueries})
- * @returns {string} The query string
- */
-const generateQueryString = (queries) =>
-    queries.map(({ name, value }) => `${name}=${value}`).join("&");
-
-/**
- * Update the URL
- */
-const updateURL = () => {
-    const dateCreated = queries.find(({ name }) => name === "dateCreated");
-    if (dateCreated) {
-        dateCreated.value = encodeURIComponent(new Date().toISOString());
-    }
-    window.history.replaceState({ originalSearch }, "", link);
-    applink.href = deepLink;
-};
-
-/**
- * When the checkbox is clicked
- */
-const checkClicked = () => updateURL();
-
-const queries = getSearchQueries(originalSearch);
-
-const link = window.location.pathname + `?${generateQueryString(queries)}`;
-const deepLink = `find://type=list&${generateQueryString(queries)}`;
 
 const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
 });
 
 window.onload = () => {
-    if (!originalSearch) {
-        console.log("No search query.");
-
-        titleElement.innerText = "No Title";
-        descriptionElement.innerText = "No Description";
-
-        wordsContainer.classList.add("hidden");
-        updateCreationDateCheckbox.classList.add("hidden");
-
-        return;
-    }
 
     titleElement.innerText = params.title ?? "Untitled";
     descriptionElement.innerText = params.description ?? "No Description";
-    const color = parseInt(params.color, 10)
 
+    const kind = params.kind ?? "";
+    const data = params.data ?? "";
+    console.log(data);
 
-
-    const hex = d => Number(d).toString(16).padStart(2, '0')
-    const colorString = "#00" + hex(color)
-    
-    // const colorString = "#" + (color.toString(16)).slice(2);
-    // console.log(color)
-    console.log(colorString)
-    r.style.setProperty('--text', colorString);
-
-
-
-    const wordsParameter = queries.find(({ name }) => name === "words");
-    if (wordsParameter) {
-        const words = wordsParameter.value.split(",").map(decodeURIComponent);
-        console.log(words);
-
-        words.forEach((word) => {
-            const p = document.createElement("p");
-            p.classList.add("word");
-            p.innerText = word;
-
-            wordsList.appendChild(p);
-        });
-    }
+    const payload = `?type=filter&kind=${kind}&data=${encodeURIComponent(data)}`;
+    const deepLink = `findapp://${payload}`;
+    console.log(deepLink);
 
     applink.href = deepLink;
 };
